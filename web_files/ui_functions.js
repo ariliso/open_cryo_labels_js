35b1d0ec2198a0e0f849a8b515070b3944ec1639
Ariel Lisogorsky <alisogor@uwaterloo.ca>
2021-08-06 00:12:42 -0400

    Major UI improvements

--- web_files/ui_functions.js
+++ web_files/ui_functions.js
@@ -1,6 +1,10 @@
 const default_sample_sets = JSON.parse(document.getElementById('data-label-sets').innerText);
-let labelsLoaded = [""];
+let labelsLoaded;
 
+
+// --------- Set Management Functions ----------------
+
+// ## file management
 
 function loadLabels(contents) {
   labelsLoaded = contents.split(/\r?\n/);
@@ -21,6 +25,19 @@ function readLabelFile(e) {
   reader.readAsText(file);
 }
 
+function getCurrentLabelSet() {
+
+  if (document.getElementById('label-set-box').value == 'custom'){
+    return  new Array(
+      document.getElementById('txt-custom-label-set').value
+    );
+  } else{
+    //TODO extract checkbox here
+    return default_sample_sets[document.getElementById('label-set-box').value];
+  }
+}
+
+//-------------- Initial Setup of UI ----------------------
 
 function populateUI(){
 
@@ -35,49 +52,117 @@ function populateUI(){
     set_selector.add(set_sel_option);
   }
 
-  document.getElementById('label-set-box')
-  .addEventListener('change', updateSampleSet, false);
+  
 
   // fix date
   document.getElementById('in-date-select').value =
     new Date().toISOString().split('T')[0];
 
+  // ###Event Listeners:####
+
+
+  document.getElementById('label-set-box')
+    .addEventListener('change', updateSampleSettings, false);
   // add file listener
   
   document.getElementById('label-input')
-  .addEventListener('change', readLabelFile, false);
+    .addEventListener('change', readLabelFile, false);
 
 }
 
-function updateSampleSet(e) {
 
-  //check if we should show or hide the custom text box
+// --------------- UI Updating Functions ----------------------------------
+function updateSampleSettings(e) {
+  
+  //check if we should show or hide the custom text box (Custom Label)
   if (document.getElementById('label-set-box').value == 'custom'){
-    document.getElementById('txt-custom-label-set').style.display = "block"
+
+    document.getElementById('in-set-custom-label').style.display = "block";
+    updateLabels();
+    return;
+  
+  }
+  // if a sample set is selected we can continue
+
+  // hide custom box;
+  document.getElementById('in-set-custom-label').style.display = "none"
+  
+  //look at all available samples in set selected
+  let full_selected_sample_set =
+    default_sample_sets[document.getElementById('label-set-box').value];
+
+  
+  // id multiselect div
+  let multiselect_div = document.getElementById("in-set-multi-check")
+
+  // if there are multiple label sets in selection, show selector block
+  if (full_selected_sample_set.length>1){
+    multiselect_div.style.display = "block"
+    populateLabelSetSelector(full_selected_sample_set)
   } else{
-    document.getElementById('txt-custom-label-set').style.display = "none"
+    multiselect_div.style.display = "none"
+    multiselect_div.lastElementChild().textContent = ''
   }
   updateLabels()
 }
 
-function getCurrentLabelSet() {
+function  populateLabelSetSelector(labelSets) {
 
-  if (document.getElementById('label-set-box').value == 'custom'){
-    return  new Array(
-      document.getElementById('txt-custom-label-set').value
-    );
-  } else{
-    return default_sample_sets[document.getElementById('label-set-box').value];
+  let selectorList = document.getElementById('in-set-multi-check-list');
+  
+  // wipe all elements inside
+  selectorList.textContent = ''
+
+  //iterate over sample list
+  for (let set_i = 0; set_i < labelSets.length; set_i++) {
+    let label_set_name = labelSets[set_i];
+
+    let new_label = document.createElement("label");
+    let new_checkbox = document.createElement("input");
+    let new_li = document.createElement("li")
+
+    new_checkbox.type = "checkbox"
+    new_checkbox.checked = labelSets.length< 2; //defaults to off if too many label types
+    new_checkbox.value = label_set_name;
+    new_checkbox.name = "in-set-chk-boxes";
+    new_checkbox.id = "in-set-chk-box-" + set_i+1;
+
+    new_label.classList.add("in-set-check-label");
+    new_label.textContent = label_set_name;
+    new_label.htmlFor = "in-set-chk-box-" + set_i+1;
+
+    new_li.appendChild(new_checkbox);
+    new_li.appendChild(new_label);
+
+    selectorList.appendChild(new_li);
+
   }
 }
 
+
 function updateLabels(e) {
 
+  if (typeof(labelsLoaded) == "undefined"){
+    return;
+  };
+
+  //update based on document
+  let name_list = labelsLoaded;
+  let label_set = getCurrentLabelSet();
+  let attn = document.getElementById("in-txt-attn").value;
+  let date = document.getElementById('in-date-select').value;
+  let skip_start = document.getElementById("in-skip-start").value
+  let page_break_set = document.getElementById("in-bool-sets-break").checked;
+
+  //fill labels
+
   populateLabels(
-    labelsLoaded,
-    getCurrentLabelSet(),
-    "Ari", //TODO: Replace placeholder
-    document.getElementById('in-date-select').value
+    name_list,
+    label_set,
+    attn, //TODO: Replace placeholder
+    date,
+    skip_start,
+    page_break_set
   )
 }
 
